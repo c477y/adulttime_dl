a793f3eb8ecf3b3f6e39518fa33836481c8a0cac
c477y <c477y@pm.me>
2023-12-09 19:26:58 +0530

    BUGFIX: * Remove `search_by_actor` and `search_by_movie` in AdultTime and use generic implementation from AlgoliaClient.

--- lib/adulttime_dl/net/adulttime_index.rb
+++ lib/adulttime_dl/net/adulttime_index.rb
@@ -5,20 +5,6 @@ module AdultTimeDL
     class AdultTimeIndex < AlgoliaClient
       INDEX_NAME = "all_scenes_latest_desc"
 
-      def search_by_actor(_actor_id, actor_name, **_opts)
-        facet_filters = { facetFilters: default_facet_filters << "actors.name:#{actor_name}" }
-        query = default_options.merge(facet_filters)
-        resp = index.search("", query)
-        AdultTimeDL.logger.error("[EMPTY RESULT] #{actor_name}") if resp[:hits].empty?
-        make_struct(resp[:hits])
-      end
-
-      def search_by_movie(movie_id, movie_name, **_opts)
-        resp = index.search("", { filters: "movie_id:#{movie_id}" })
-        AdultTimeDL.logger.error("[EMPTY RESULT] #{movie_name}") if resp[:hits].empty?
-        make_struct(resp[:hits])
-      end
-
       private
 
       def index
